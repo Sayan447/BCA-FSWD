082391a3eecbdcee6625f73a6aefec14da2629a9
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2025-02-27 19:08:50 +0530

    porps drilling rendering

---
+++ 15feb2025react/My-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+// what is functional component:-
+// it is a normal js function that return  some jsx
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <div className='border my-2 mx-5 text-center'>App</div>
+    </>
+  );
+};
+
+export default App;
